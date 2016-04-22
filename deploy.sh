33752b35477d294cd650b8ee7bd3c7974fac8b5f
Jake Swenson <jswenson@godaddy.com>
2016-04-22 07:51:09 -0700

    remove dup decrypt

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@ if [ -z "$GPG_PRIVATE_KEY_ENCRYPTION_IV" ]; then
     exit 0
 fi
 
-openssl aes-256-cbc -K $GPG_PRIVATE_KEY_ENCRYPTION_KEY -iv $GPG_PRIVATE_KEY_ENCRYPTION_IV \
-    -in gpg/paradoxical-io-private.gpg.enc -out gpg/paradoxical-io-private.gpg -d
-
 if [ -n "$TRAVIS_TAG" ]; then
     echo "Deploying release version"
     mvn clean deploy --settings settings.xml -DskipTests -P release -Drevision=''
