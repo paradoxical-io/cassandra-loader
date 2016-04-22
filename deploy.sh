1566a392a13697a9a662a09cb8a32abd35c6bd3d
Jake Swenson <jswenson@godaddy.com>
2016-04-22 07:50:47 -0700

    adding new build system

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  mvn clean deploy --settings settings.xml -DskipTests
+if [ -z "$GPG_PRIVATE_KEY_ENCRYPTION_IV" ]; then
+    exit 0
+fi
+
+openssl aes-256-cbc -K $GPG_PRIVATE_KEY_ENCRYPTION_KEY -iv $GPG_PRIVATE_KEY_ENCRYPTION_IV \
+    -in gpg/paradoxical-io-private.gpg.enc -out gpg/paradoxical-io-private.gpg -d
+
+if [ -n "$TRAVIS_TAG" ]; then
+    echo "Deploying release version"
+    mvn clean deploy --settings settings.xml -DskipTests -P release -Drevision=''
+    exit $?
+elif [ "$TRAVIS_BRANCH" = "master" ]; then
+    echo "Deploying snapshot version"
+    mvn clean deploy --settings settings.xml -DskipTests
+    exit $?
 fi
--- perform-release.sh
+++
@@ -1,1 +0,0 @@
-mvn release:perform -P sign -P shade -s settings.xml
